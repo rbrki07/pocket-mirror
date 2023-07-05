88f207289df4817a5eb67c265debd40aa80011a4
René Wilby <rbrki07@gmail.com>
2023-07-05 20:07:49 +0200

    fix: could not read license text on android using dark mode

--- screens/ThirdPartyLibsDetailScreen.js
+++ screens/ThirdPartyLibsDetailScreen.js
@@ -1,18 +1,12 @@
 // @ts-check
 import React, { useLayoutEffect } from 'react'
 import { StyleSheet } from 'react-native'
-import { useTheme } from './../hooks/useTheme'
 import { WebView } from 'react-native-webview'
-// eslint-disable-next-line no-unused-vars
-import typedefs from './../typedefs'
 
 /**
  * @returns {Object} ThirdPartyLibsDetailScreen
  */
 const ThirdPartyLibsDetailScreen = ({ route, navigation }) => {
-  const theme = useTheme()
-  const styles = themedStyles(theme)
-
   const { uri, license } = route.params.item
 
   useLayoutEffect(() => {
@@ -24,18 +18,11 @@ const ThirdPartyLibsDetailScreen = ({ route, navigation }) => {
   return <WebView textZoom={250} style={styles.container} source={{ uri }} />
 }
 
-/**
- * @param {typedefs.Theme} currentTheme
- *
- * @returns {Object}
- */
-const themedStyles = (currentTheme) =>
-  StyleSheet.create({
-    // eslint-disable-next-line react-native/no-unused-styles
-    container: {
-      backgroundColor: currentTheme.backgroundColor,
-      flex: 1,
-    },
-  })
+const styles = StyleSheet.create({
+  // eslint-disable-next-line react-native/no-unused-styles
+  container: {
+    flex: 1,
+  },
+})
 
 export { ThirdPartyLibsDetailScreen }
