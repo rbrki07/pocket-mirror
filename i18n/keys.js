2848820596787b1d999cd66dbb2351789d06dbea
René Wilby <rbrki07@gmail.com>
2023-07-10 16:22:42 +0200

    feat: add imprint screen (#2)

--- i18n/keys.js
+++ i18n/keys.js
@@ -1,8 +1,18 @@
 // @ts-check
+// AboutScreen
 export const I18N_KEY_SCREEN_ABOUT_HEADER_TITLE = 'screen_about_header_title'
 export const I18N_KEY_SCREEN_ABOUT_VERSION = 'screen_about_version'
+// ImprintScreen
 export const I18N_KEY_SCREEN_IMPRINT_HEADER_TITLE =
   'screen_imprint_header_title'
+export const I18N_KEY_SCREEN_IMPRINT_RESPONSIBLE = 'screen_imprint_responsible'
+export const I18N_KEY_SCREEN_IMPRINT_CONTACT = 'screen_imprint_contact'
+export const I18N_KEY_SCREEN_IMPRINT_PHONE = 'screen_imprint_phone'
+export const I18N_KEY_SCREEN_IMPRINT_MAIL = 'screen_imprint_mail'
+export const I18N_KEY_SCREEN_IMPRINT_DISPUTE_RESOLUTION =
+  'screen_imprint_dispute_resolution'
+export const I18N_KEY_SCREEN_IMPRINT_ODR = 'screen_imprint_odr'
+// MenuScreen
 export const I18N_KEY_SCREEN_MENU_HEADER_TITLE = 'screen_menu_header_title'
 export const I18N_KEY_SCREEN_MENU_ITEM_SETTING = 'screen_menu_item_setting'
 export const I18N_KEY_SCREEN_MENU_ITEM_IMPRINT = 'screen_menu_item_imprint'
@@ -11,16 +21,20 @@ export const I18N_KEY_SCREEN_MENU_ITEM_THIRD_PARTY_LIBS =
   'screen_menu_item_third_party_libs'
 export const I18N_KEY_SCREEN_MENU_ITEM_ABOUT = 'screen_menu_item_about'
 export const I18N_KEY_SCREEN_MENU_CLOSE_BUTTON = 'screen_menu_close_button'
+// PrivacyScreen
 export const I18N_KEY_SCREEN_PRIVACY_HEADER_TITLE =
   'screen_privacy_header_title'
+// SettingScreen
 export const I18N_KEY_SCREEN_SETTING_HEADER_TITLE =
   'screen_setting_header_title'
 export const I18N_KEY_SCREEN_SETTING_ITEM_WHITE_BALANCE =
   'screen_setting_item_white_balance'
+// ThirdPartyLibsScreen
 export const I18N_KEY_SCREEN_THIRD_PARTY_LIBS_HEADER_TITLE =
   'screen_third_party_libs_header_title'
 export const I18N_KEY_SCREEN_THIRD_PARTY_LIBS_INFO =
   'screen_third_party_libs_info'
+// WelcomeScreen
 export const I18N_KEY_SCREEN_WELCOME_HEADLINE = 'screen_welcome_headline'
 export const I18N_KEY_SCREEN_WELCOME_ACTIVATE_CAMERA_ADVICE =
   'screen_welcome_activate_camera_advice'
@@ -30,6 +44,7 @@ export const I18N_KEY_SCREEN_WELCOME_OPEN_SETTINGS_ADVICE =
   'screen_welcome_open_settings_advice'
 export const I18N_KEY_SCREEN_WELCOME_OPEN_SETTINGS_BUTTON =
   'screen_welcome_open_settings_button'
+// PMBrightnessChanger
 export const I18N_KEY_COMPONENT_PM_BRIGHTNESS_CHANGER_REQUEST_PERMISSION_DIALOG_TITLE =
   'component_pm_brightness_changer_request_permission_dialog_title'
 export const I18N_KEY_COMPONENT_PM_BRIGHTNESS_CHANGER_REQUEST_PERMISSION_DIALOG_MESSAGE =
--- screens/ImprintScreen.js
+++ screens/ImprintScreen.js
@@ -1,11 +1,29 @@
 // @ts-check
 import React, { useLayoutEffect } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import {
+  Linking,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from 'react-native'
 import { useTheme } from './../hooks/useTheme'
 import { i18n } from '../i18n'
-import { I18N_KEY_SCREEN_IMPRINT_HEADER_TITLE } from '../i18n/keys'
+import {
+  I18N_KEY_SCREEN_IMPRINT_CONTACT,
+  I18N_KEY_SCREEN_IMPRINT_DISPUTE_RESOLUTION,
+  I18N_KEY_SCREEN_IMPRINT_HEADER_TITLE,
+  I18N_KEY_SCREEN_IMPRINT_MAIL,
+  I18N_KEY_SCREEN_IMPRINT_ODR,
+  I18N_KEY_SCREEN_IMPRINT_PHONE,
+  I18N_KEY_SCREEN_IMPRINT_RESPONSIBLE,
+} from '../i18n/keys'
 // eslint-disable-next-line no-unused-vars
 import typedefs from './../typedefs'
+
+const PHONE_NUMBER = '+4943153025887'
+const MAIL = 'kontakt@rene-wilby.de'
+const EU_ODR_URL = 'https://ec.europa.eu/consumers/odr/'
 
 /**
  * @returns {Object} ImprintScreen
@@ -21,11 +39,41 @@ const ImprintScreen = ({ navigation }) => {
   }, [navigation])
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>
-        {i18n.t(I18N_KEY_SCREEN_IMPRINT_HEADER_TITLE)}
+    <ScrollView style={styles.container}>
+      <Text style={[styles.bold, styles.text]}>
+        {i18n.t(I18N_KEY_SCREEN_IMPRINT_RESPONSIBLE)}
       </Text>
-    </View>
+      <Text style={styles.text}>{'René Wilby'}</Text>
+      <Text style={styles.text}>{'Burbarg 15'}</Text>
+      <Text style={styles.text}>{'24226 Heikendorf'}</Text>
+      <Text style={[styles.bold, styles.text]}>
+        {i18n.t(I18N_KEY_SCREEN_IMPRINT_CONTACT)}
+      </Text>
+      <TouchableOpacity onPress={() => Linking.openURL(`tel:${PHONE_NUMBER}`)}>
+        <Text style={styles.text}>
+          <Text>{`${i18n.t(I18N_KEY_SCREEN_IMPRINT_PHONE)}: `}</Text>
+          <Text style={styles.link}>{PHONE_NUMBER}</Text>
+          <Text>{'.'}</Text>
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => Linking.openURL(`mailto:${MAIL}`)}>
+        <Text style={styles.text}>
+          <Text>{`${i18n.t(I18N_KEY_SCREEN_IMPRINT_MAIL)}: `}</Text>
+          <Text style={styles.link}>{MAIL}</Text>
+          <Text>{'.'}</Text>
+        </Text>
+      </TouchableOpacity>
+      <Text style={[styles.bold, styles.text]}>
+        {i18n.t(I18N_KEY_SCREEN_IMPRINT_DISPUTE_RESOLUTION)}
+      </Text>
+      <TouchableOpacity onPress={() => Linking.openURL(EU_ODR_URL)}>
+        <Text style={styles.text}>
+          <Text>{`${i18n.t(I18N_KEY_SCREEN_IMPRINT_ODR)}: `}</Text>
+          <Text style={styles.link}>{EU_ODR_URL}</Text>
+          <Text>{'.'}</Text>
+        </Text>
+      </TouchableOpacity>
+    </ScrollView>
   )
 }
 
@@ -37,15 +85,27 @@ const ImprintScreen = ({ navigation }) => {
 const themedStyles = (currentTheme) =>
   StyleSheet.create({
     // eslint-disable-next-line react-native/no-unused-styles
+    bold: {
+      fontWeight: 'bold',
+      marginBottom: 4,
+      marginTop: 12,
+    },
+    // eslint-disable-next-line react-native/no-unused-styles
     container: {
-      alignItems: 'center',
       backgroundColor: currentTheme.backgroundColor,
       flex: 1,
-      justifyContent: 'center',
+      margin: 8,
+    },
+    // eslint-disable-next-line react-native/no-unused-styles
+    link: {
+      textDecorationColor: currentTheme.textColor,
+      textDecorationLine: 'underline',
     },
     // eslint-disable-next-line react-native/no-unused-styles
     text: {
       color: currentTheme.textColor,
+      fontSize: 16,
+      lineHeight: 24,
     },
   })
 
