21ecc8ea4d1863ab70677a9a9a76d8910d924cae
René Wilby <rbrki07@gmail.com>
2023-07-13 21:41:49 +0200

    chore: bump version of expo to 49.0.3

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 // @ts-check
 import React from 'react'
+import { LogBox } from 'react-native'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { store, persitor } from './store'
 import { PMMain } from './components/PMMain'
+
+LogBox.ignoreLogs(['new NativeEventEmitter'])
 
 export default function App() {
   return (
