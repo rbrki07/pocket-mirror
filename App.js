7e3562aa721d8b77c24f1d85cd0915b02e43d97e
René Wilby <rbrki07@gmail.com>
2024-07-09 10:00:41 +0200

    chore: remove log ignore for reanimated

--- App.js
+++ App.js
@@ -1,15 +1,10 @@
 // @ts-check
 import React from 'react'
-import { LogBox } from 'react-native'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 
 import { PMMain } from './components/PMMain'
 import { store, persitor } from './store'
-
-LogBox.ignoreLogs([
-  "[Reanimated] Couldn't determine the version of the native part of Reanimated.",
-])
 
 export default function App() {
   return (
